d16e86b968cd03698048a487d7cf944d8646ff41
j0nspa <loldexterity@gmail.com>
2022-10-10 19:26:07 +0300

    Getting joke of the day using axios library from api.jokes.one/jod

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+const URL = "https://api.jokes.one/jod";
 
 function App() {
+
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    axios.get(URL)
+      .then ((response) => {
+        // console.log(response)
+        const joke = response.data.contents.jokes[0].joke;
+        setTitle(joke.title);
+        setText(joke.text);
+    }).catch(error => {
+        console.log(error);
+        alert("Retrieving joke of the day failed.");
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: 50}}>
+      <h1>Joke of the day</h1>
+      <h3>{title}</h3>
+      <p>{text}</p>
     </div>
+
   );
 }
 
